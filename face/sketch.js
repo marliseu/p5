fb287190015d480939ddb56070612c165de2e63c
Marlise Urscheler <marlise.au@gmail.com>
2016-09-18 16:27:16 -0400

    Ball grows with volume

--- face/sketch.js
+++ face/sketch.js
@@ -1,11 +1,25 @@
-var circleSize;
+var mic;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	circleSize = windowWidth/4;
-	ellipse(windowWidth/2, windowHeight/2, circleSize, circleSize);
+  createCanvas(windowWidth, windowHeight);
+
+  // Create an Audio input
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
 }
 
 function draw() {
+  background(255);
 
+  // Get the overall volume (between 0 and 1.0)
+  var vol = mic.getLevel();
+  fill(0, 255, 0);
+  stroke(0);
+
+  // Draw an ellipse with height based on volume
+  var h = map(vol, 0, 1, height/4, height);
+  ellipse(width/2, height/2, h, h);
 }
